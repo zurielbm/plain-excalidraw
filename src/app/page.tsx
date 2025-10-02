4a86dbf035e541d56b90fdc34f3490d967371ffc
Zuriel Baxcajay <zbaxcajay@gmail.com>
2025-10-02 17:34:07 +0000

    Links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,20 @@ export default function Home() {
         <h1 className="text-6xl font-bold tracking-tight text-foreground">
           Hello, world!
         </h1>
+        <div className="flex flex-col items-center justify-center">
         <Link
           href="/test-math"
           className="text-xl text-blue-500 hover:underline"
         >
           Go to Test Math Page
         </Link>
+        <Link
+          href="/est-common"
+          className="text-xl text-blue-500 hover:underline"
+        >
+          Go to Test Common Page
+        </Link>
+        </div>
       </div>
     </main>
   );
