2e752ae54aea44011afd51f3564d8556f985866b
zurielbm <58864716+zurielbm@users.noreply.github.com>
2025-10-03 16:05:02 -0400

    main error fixed

--- src/lib/mermaid-to-excalidraw/dist/interfaces.ts
+++
@@ -1,7 +0,0 @@
-import type { ExcalidrawElementSkeleton } from "../../data/transform";
-import type { BinaryFiles } from "../../types";
-
-export interface MermaidToExcalidrawResult {
-  elements: ExcalidrawElementSkeleton[];
-  files: BinaryFiles | null;
-}
